9d66b0a0fb5f115dbd5bf7670b08ee720f8df3d9
TG2408 <tanmaygupta2408@gmail.com>
2022-01-03 18:26:12 +0530

    database connection established

--- src/index.server.js
+++ src/index.server.js
@@ -1,13 +1,23 @@
 // modules
 const express = require('express');
-const env = require("dotenv");
-const bodyParser = require('body-parser');
+const env = require("dotenv");                  // dotenv is used to include .env key and values
+const bodyParser = require('body-parser');      // for parsing incoming json data
+const mongoose = require('mongoose');          // for designing database schema
 
 // initializing express
 const app = express();
 
 // Environment variables
 env.config();
+
+// mongoDB connection
+// mongodb+srv://root:<password>@cluster0.qad4m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.qad4m.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`
+).then(() => {
+    console.log("Database Connected");
+});
+
 
 app.use(bodyParser());
 
