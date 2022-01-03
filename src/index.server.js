07a79f31ee650dc12d399d00bdd6375780104aed
TG2408 <tanmaygupta2408@gmail.com>
2022-01-03 13:06:00 +0530

    demo get & post method made

--- src/index.server.js
+++ src/index.server.js
@@ -8,6 +8,20 @@ const app = express();
 // Environment variables
 env.config();
 
+app.use(express.json());
+
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: "Hello from server"
+    })
+})
+
+app.post('/data', (req, res, next) => {
+    res.status(200).json({
+        message: req.body
+    })
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`server is running on ${process.env.PORT}`);
 })
