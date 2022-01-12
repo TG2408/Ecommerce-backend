14f5d580406955ba8f480bf2eb1f89ad9c9c0612
TG2408 <tanmaygupta2408@gmail.com>
2022-01-12 17:43:26 +0530

    validation working but optmization needed

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,24 @@
 const express = require('express');
-const { signup, signin, requireSignin } = require('../controller/auth');
+const { signup, signin } = require('../controller/auth');
+const { check } = require('express-validator');
 const router = express.Router();
 
 
 router.post('/signin', signin);
-router.post('/signup', signup);
+router.post('/signup', [
+    check('firstname')
+    .notEmpty()
+    .withMessage('firstname is required'),
+    check('lastname')
+    .notEmpty()
+    .withMessage('lastname is required'),
+    check('email')
+    .isEmail()
+    .withMessage('Valid Email is required'),
+    check('password')
+    .isLength({ min: 6 })
+    .withMessage('password must be atleast 6 character long')
+], signup);
 
 // router.post('/profile', requireSignin,(req, res) => {
 //      res.status(200).json({ user: req.user });
