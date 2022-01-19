485f34681fc99ce22910a2dc8942e41a5077bf1c
TG2408 <tanmaygupta2408@gmail.com>
2022-01-20 00:08:16 +0530

    some updates

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/'})
+const shortid = require('shortid');
+const path = require('path');
 
 // initializing express router
 const router = express.Router();
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '../uploads/')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, shortid.generate() + '-' + file.originalname)
+    }
+  })
+
+const upload = multer({ storage });
 
 // import middleware module
 const { requireSignin, adminMiddleware } = require('../common-middleware/index');
